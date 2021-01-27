8e7677e727e3ad321e8037304d782ef9f72cfb65
Brekke Green <brekke.andrew.green@gmail.com>
2021-01-27 12:16:50 -0500

    Completed import_db.sql file and tested successfully

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -12,9 +14,9 @@ CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    author INTEGER NOT NULL,
+    author_id INTEGER NOT NULL,
 
-    FOREIGN KEY (author) REFERENCES users(id)
+    FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS question_follows;
@@ -29,3 +31,42 @@ CREATE TABLE question_follows (
 );
 
 DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    body TEXT NOT NULL,
+    question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    liker_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (liker_id) REFERENCES users(id)
+);
+
+INSERT INTO 
+    users (fname, lname)
+VALUES
+    ('Brekke', 'Green'), 
+    ('Chris', 'Mann');
+
+INSERT INTO 
+    questions (title, body, author_id)
+VALUES
+    ('Bundle install', 'How do you bundle install?', (SELECT id FROM users WHERE fname = 'Chris' AND lname = 'Mann'));
+
+INSERT INTO 
+    replies (body, question_id, author_id)
+VALUES
+    ('DO NOT BUNDLE INSTALL', (SELECT id FROM questions WHERE title = 'Bundle install'), (SELECT id FROM users WHERE fname = 'Brekke' AND lname = 'Green'));
