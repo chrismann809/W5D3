3c1a160975dca16659a9f9d079d2f5b7d1cf19f3
Chris Mann <chrismann809@gmail.com>
2021-01-27 11:42:19 -0500

    started creating database in import_db.sql

---
+++ aa_questions/import_db.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author INTEGER NOT NULL,
+
+    FOREIGN KEY (author) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_follows;
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    follower_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (follower_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS replies;
