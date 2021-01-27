9827908e9b8f760681c6adf5f4ab1c7a8f34f3c5
Chris Mann <chrismann809@gmail.com>
2021-01-27 14:52:48 -0500

    created Questions class, User#authored_questions, Questions::find_by_author_id

--- aa_questions/questions.rb
+++ aa_questions/questions.rb
@@ -64,8 +64,88 @@ class User
                 fname = ? AND
                 lname = ?
         SQL
-        User.new(data.first)
+        return nil if data.empty?
+        data.map { |datum| User.new(datum) }
     end 
 
+    def authored_questions
+        data = QuestionsDatabase.instance.execute(<<-SQL, self.id)
+            SELECT
+                *
+            FROM 
+                questions
+            JOIN
+                users ON questions.author_id = users.id
+            WHERE
+                users.id = ?;
+        SQL
+        return nil if data.empty?
+        data.map { |datum| Question.new(datum) }
+    end
+end
 
+class Question
+    attr_accessor :id, :title, :body, :author_id
+
+    def self.all
+        data = QuestionsDatabase.instance.execute('SELECT * FROM questions')
+        data.map { |datum| Question.new(datum) }
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
+    end
+
+    def create
+        raise 'id already in use' if self.id
+        QuestionsDatabase.instance.execute(<<-SQL, self.title, self.body, self.author_id)
+            INSERT INTO
+                questions (title, body, author_id)
+            VALUES
+                (?, ?, ?)
+        SQL
+        self.id = QuestionsDatabase.instance.last_insert_row_id
+    end
+
+    def update
+        raise 'id not valid' unless self.id
+        QuestionsDatabase.instance.execute(<<-SQL, self.title, self.body, self.author_id, self.id)
+            UPDATE
+                questions
+            SET
+                title = ?, body = ?, author_id = ?
+            WHERE
+                id = ?;
+        SQL
+    end
+
+    def self.find_by_author_id(author_id)
+        data = QuestionsDatabase.instance.execute(<<-SQL, author_id)
+            SELECT
+                *
+            FROM
+                questions
+            WHERE
+                author_id = ?;
+        SQL
+        return nil if data.empty?
+        data.map { |datum| Question.new(datum) }
+    end
+
+    def author
+        data = QuestionsDatabase.instance.execute(<<-SQL, self.id)
+            SELECT
+                *
+            FROM
+                users
+            JOIN
+                questions ON users.id = questions.author_id
+            WHERE
+                questions.id = ?;
+        SQL
+        User.new(data.first)
+    end
 end
